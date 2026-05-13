520475bf1c9548ea75d643b9c513add833a4a377
BelliaNino <bellianino1996@gmail.com>
2026-05-13 17:05:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>hello world</h1>
+    </>
+  )
+}
+
+export default App
